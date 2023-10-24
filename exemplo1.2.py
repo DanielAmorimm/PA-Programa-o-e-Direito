7ea04fa2bbcc8c2999b0608b6a8ee2611ef76fb4
DanielAmorimm <141172716+DanielAmorimm@users.noreply.github.com>
2023-10-24 14:06:53 -0300

    Add files via upload

---
+++ exemplo1.2.py
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+numero = 69
+
+nome = "Pazuello"
+
+bootelano = True
+
+
+
+
+a = 10
+b = 5
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao= a / b
+
+va = True
+vb = False
+
+r1= va and vb
+r2= va or vb
+r3= not va 
+
+c = 3
+
+r4 = c > 4
+r5 = c == 4
+r6 = c != 5
+
+
